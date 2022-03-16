6fffe53e5ff12ab6d8747454623a74b036f1c950
Maicon William <maicon.w.a@gmail.com>
2022-03-16 20:58:03 -0300

    fixed service worker, rebuilding react app

--- blogapi/src/index.js
+++ blogapi/src/index.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
 import './index.css';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import App from './App';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import LocalServiceWorkerRegister from './sw-register';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
-
-const routing = (
-  <Router>
-    <React.StrictMode>
-      <Header>
-      <Switch>
-          <Route exact path="/" component={App} />
-      </Switch>
-      <Footer/>
-      </Header>
-    </React.StrictMode>
-  </Router>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-serviceWorker.register();
-LocalServiceWorkerRegister();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
